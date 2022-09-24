cfb92980a909f8a1b7b0c101a0e70ec36db734d6
Justas Kavarskas <justas@digitalguest.com>
2022-09-24 19:03:46 +0200

    [hotfix, toggle-unicode-admin], added a checkbox to toggle Unicode usage for sms

--- src/components/Snake.vue
+++ src/components/Snake.vue
@@ -1,27 +1,25 @@
 <template>
-  <div @keydown="move" tabindex="0" id="123" class="snake">Snake</div>
+	<div @keydown="move" tabindex="0" id="123" class="snake">Snakee</div>
 </template>
 
 <script>
-import vue from "vue";
-export default vue.extend({
-  name: "Snake",
-  methods: {
-    move(event) {
-      if (event.key === "ArrowUp" || event.key === "ArrowDown")
-        this.$emit("upDown", event.key);
-      if (event.key === "ArrowLeft" || event.key === "ArrowRight")
-        this.$emit("leftRight", event.key);
-    },
-  },
-});
+	import vue from "vue";
+	export default vue.extend({
+		name: "Snake",
+		methods: {
+			move(event) {
+				if (event.key === "ArrowUp" || event.key === "ArrowDown") this.$emit("upDown", event.key);
+				if (event.key === "ArrowLeft" || event.key === "ArrowRight") this.$emit("leftRight", event.key);
+			},
+		},
+	});
 </script>
 
 <style lang="scss" scoped>
-.snake {
-  width: 50px;
-  height: 50px;
-  background: purple;
-  text-align: center;
-}
+	.snake {
+		width: 50px;
+		height: 50px;
+		background: purple;
+		text-align: center;
+	}
 </style>
